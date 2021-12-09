71b62e7089b954bfd2a88e854fde8cbfbde2e31f
omar4321 <akib2000omar@gmail.com>
2021-12-10 01:54:41 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# doctor-protal-server-side" 
---
+++ index.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient } = require('mongodb');
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = ` mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ksj3s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db('doctors_portal');
+    console.log('database connected successfully');
+    const appointmentsCollection = database.collection('appointments');
+    const userCollection = database.collection('users');
+
+    app.get('/appointments', async (req, res) => {
+      const email = req.query.email;
+      const date = new Date(req.query.date).toLocaleDateString();
+      const query = { email: email, date: date };
+      console.log(query);
+      const cursor = appointmentsCollection.find(query);
+      const appointments = await cursor.toArray();
+      res.json(appointments);
+    });
+
+    app.post('/appointments', async (req, res) => {
+      const appointment = req.body;
+      const result = await appointmentsCollection.insertOne(appointment);
+      // console.log(result);
+      res.json(result);
+    });
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      console.log(result);
+      res.json(result);
+    });
+    app.put('/users', async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const options = { upsert: true };
+      const updateDoc = { $set: user };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      res.json(result);
+    });
+
+    app.put('/users/admin', async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const options = { upsert: true };
+      const updateDoc = { $set: user };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      res.json(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at ${port}`);
+});
